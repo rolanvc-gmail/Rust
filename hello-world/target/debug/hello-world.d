57ed461a9b7a05fd10acad5e9b852f607a326598
Rolan Veron Cruz <rolanvc@gmail.com>
2021-11-15 13:21:58 +0800

    minor revisions

--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\rolan\Rust\hello-world\target\debug\hello-world.exe: C:\Users\rolan\Rust\hello-world\src\main.rs
--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -6,11 +6,11 @@ use to_do::structs::pending::Pending;
 
 fn main() {
     let done: Done = Done::new("shopping");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status);
+    println!("Title is:  {}", done.super_struct.title);
+    println!("Status is: {}", done.super_struct.status);
 
     let pending = Pending::new("laundry");
-    println!("{}", pending.super_struct.title);
-    println!("{}", pending.super_struct.status);
+    println!("Title is:  {}", pending.super_struct.title);
+    println!("Status is: {}", pending.super_struct.status);
 
 }
