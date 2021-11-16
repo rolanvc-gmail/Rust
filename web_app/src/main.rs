248e60e45802d4fef15f84f7812e765aa2337a53
Rolan Veron Cruz <rolanvc@gmail.com>
2021-11-16 19:02:04 +0800

    Completed Section 1:Setting up the Web App Structure

--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -1,35 +1,30 @@
 mod state;
+mod to_do;
+mod processes;
 
 use std::env;
-use state::{write_to_file, read_file};
+use state::read_file;
 use serde_json::value::Value;
-use serde_json::{Map, json};
-/*
-mod to_do;
-
-use to_do::structs::done::Done;
-use to_do::structs::pending::Pending;
+use serde_json::Map;
+use to_do::to_do_factory;
+use processes::process_input;
 
 fn main() {
-    let done: Done = Done::new("shopping");
-    println!("Title is:  {}", done.super_struct.title);
-    println!("Status is: {}", done.super_struct.status);
-
-    let pending = Pending::new("laundry");
-    println!("Title is:  {}", pending.super_struct.title);
-    println!("Status is: {}", pending.super_struct.status);
-
-}
-*/
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let status: &String = &args[1];
+    let args: Vec <String> = env::args().collect();
+    let command: &String = &args[1];
     let title: &String = &args[2];
 
-    let mut state: Map<String, Value> = read_file(&String::from("./state.json"));
-    println!("{:?}", state);
-    state.insert(title.to_string(), json!(status));
-    write_to_file("./state.json", &mut state);
+    let state:Map<String, Value> = read_file("./state.json");
 
+    let status:String;
+    match &state.get(*&title) {
+        Some(result) =>{
+            status = result.to_string().replace('\"', "");
+        }
+        None=>{
+            status="pending".to_string();
+        }
+    }
+    let item = to_do_factory(&status, title).expect(&status);
+    process_input(item, command.to_string(), &state);
 }
